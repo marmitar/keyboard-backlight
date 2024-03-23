b947df1d1b462e3a18dc152d476bc6dda9b894b7
Tiago de Paula <tiagodepalves@gmail.com>
2024-03-22 23:56:57 -0300

    WIP: types

---
+++ test.mjs
@@ -0,0 +1,17 @@
+// const symb = Symbol("test");
+// const map = new WeakMap();
+// map.set(symb, {x: 99});
+// if (map.get(symb).x !== 99) {
+//     throw new Error("INVALID");
+// }
+
+// function sleep(ms) {
+//     return new Promise((resolve) => {
+//         setTimeout(resolve, ms);
+//     });
+// }
+// await sleep(1_000);
+// console.log("Done!");
+
+import Gdk from 'gi://Gdk';
+console.log(Gdk);
